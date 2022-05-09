d51a2eef252518a23dc79cecece7bc1873a84181
SakibFahim <sakibfahim1999@gmail.com>
2022-05-09 22:39:45 +0600

    Change in Master

--- src/new_package_2/java_class_2.java
+++ src/new_package_2/java_class_2.java
@@ -3,4 +3,46 @@ package new_package_2;
 public class java_class_2 {
     string abc;
     int ID;
+
+    string efg;
+    int roll;
+
+    public java_class_2(string abc, int ID, string efg, int roll) {
+        this.abc = abc;
+        this.ID = ID;
+        this.efg = efg;
+        this.roll = roll;
+    }
+
+    public string getAbc() {
+        return abc;
+    }
+
+    public void setAbc(string abc) {
+        this.abc = abc;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public string getEfg() {
+        return efg;
+    }
+
+    public void setEfg(string efg) {
+        this.efg = efg;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
 }
